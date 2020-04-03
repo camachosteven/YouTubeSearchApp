a048aea44d2713fd5463fe3a6fae699734ff11be
camachosteven <heatrcoks63134@gmail.com>
2020-04-03 13:51:59 -0400

    Original program

--- camera.py
+++ camera.py
@@ -17,7 +17,7 @@ class Camera(BaseCamera):
 
     @staticmethod
     def frames():
-        camera = cv2.VideoCapture(Camera().video_source)
+        camera = cv2.VideoCapture(Camera.video_source)
         if not camera.isOpened():
             raise RuntimeError('Could not start camera.')
 
